7246d1bd3d4a43502d2f27a0a161625c7441c145
Adam Wootton <ajwootto@uwaterloo.ca>
2016-03-26 20:52:38 -0400

    should work code

--- weather_data.py
+++ weather_data.py
@@ -4,14 +4,31 @@ import urllib2
 import lxml
 import lxml.html
 import serial
-
+import threading
 
 open_weather_key = "48dfc67c29ac7993c20359a807ef1308"
 open_weather_base = "http://api.openweathermap.org/data/2.5/group?appid=48dfc67c29ac7993c20359a807ef1308&units=metric&id="
 aqi_base_url = 'http://aqicn.org/city/tokyo' 
 
+
+CITY_LIST = ['tokyo', 'toronto', 'beijing', 'london', 'singapore']
+current_city_index = 0
+ELEMENTS = ['rain', 'temp', 'wind', 'aqi']
+MAX_ELEMENTS = {'rain': 20, 'temp': 40, 'wind': 75, 'aqi': 150}
+
 cities = {'tokyo': '1850147', 'toronto': '6167865', 'beijing': '1816670', 'london': '2643743', 'singapore': '1880252'}
 weather_cities = {}
+
+SERIAL_PORTS = {'rain': '/dev/ttyACM0', 'temp': '/dev/ttyACM1', 'wind': '/dev/ttyACM2', 'aqi': '/dev/ttyACM3'}
+SERIAL_COMS = {}
+
+for port in SERIAL_PORTS.keys():
+  try:
+    SERIAL_COMS[port] = serial.Serial(SERIAL_PORTS[port], 9600)
+  except:
+    print "No Arduino Found For: " + port + " " + SERIAL_PORTS[port]
+    SERIAL_COMS[port] = None
+
 
 for city in cities.keys():
   weather_cities[city] = {}
@@ -25,7 +42,6 @@ MAX_PRECIP = 20
 MAX_WIND = 75
 MAX_AQI = 150
 
-current_city = "toronto"
 
 def aqi_city(city):
   if city=='singapore':
@@ -46,8 +62,10 @@ def massage_weather(weather):
   for data in weather['list']:
     #not every city has rain returned
     rain = data.get('rain')
-    if rain:
+    if rain and rain.get('1h'):
       rain = rain['1h']
+    elif rain and rain.get('3h'):
+      rain = rain['3h']
     else:
       rain = 0
     city_data = weather_cities[data['name'].lower()]
@@ -61,10 +79,67 @@ def update_weather():
   for city in cities.keys():
     aqi_city(city)
 
+def write_serial(element, value):
+  if SERIAL_COMS[element]:
+    SERIAL_COMS[element].write(value)
+
+def update_current_city():
+  global current_city_index
+  current_city_index += 1
+  if current_city_index > len(CITY_LIST) - 1:
+    current_city_index = 0
+
 
 update_weather()
 
+def calculate_output(value, max_val):
+  return 205*value/max_val + 50
+
+
+
 print weather_cities
+
+
+
+class WeatherThread(threading.Thread):
+    def __init__(self, event):
+        threading.Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(900):
+          update_weather()
+          print weather_cities
+
+class SerialThread(threading.Thread):
+    def __init__(self, event):
+        threading.Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(5):
+          for element in ELEMENTS:
+            cur_city = CITY_LIST[current_city_index]
+            write_serial(element, 'CITY ' + cur_city)
+            value = calculate_output(float(weather_cities[cur_city][element]), MAX_ELEMENTS[element])
+            write_serial(element, value)
+          update_current_city()
+
+
+
+stop = threading.Event()
+weather_thread = WeatherThread(stop)
+weather_thread.daemon = True
+weather_thread.start()
+
+serial_thread = SerialThread(stop)
+serial_thread.daemon = True
+serial_thread.start()
+
+while True:
+  if not weather_thread.isAlive() or not serial_thread.isAlive():
+    raise Exception("WE DEAD")
+
 
 
 
