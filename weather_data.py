9f36dc8eda902e1d7debf55895162e3150ebc6fd
Adam Wootton <ajwootto@uwaterloo.ca>
2016-04-07 16:15:52 -0400

    encoding removed

--- weather_data.py
+++ weather_data.py
@@ -115,7 +115,7 @@ class SerialThread(threading.Thread):
         while not self.stopped.wait(15):
           for element in ELEMENTS:
             cur_city = CITY_LIST[current_city_index]
-            write_serial(element, "C " + cur_city.capitalize() + " " + JAP[cur_city])
+            write_serial(element, "C " + cur_city.capitalize())
             value = calculate_output(abs(float(weather_cities[cur_city][element])), MAX_ELEMENTS[element])
             write_serial(element, "P " + str(int(value)))
             write_serial(element, "V " + str(int(weather_cities[cur_city][element])))
